4b8963ca2a6af1e1cc469ca52cdab1aa6df257eb
Ewa Wydra <e.wydra@selleo.com>
2021-01-28 23:06:40 +0100

    Fix tests

--- src/components/Navbar.test.js
+++ src/components/Navbar.test.js
@@ -1,25 +1,26 @@
-import React from 'react';
-import { shallow, mount } from 'enzyme';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { mount } from "enzyme";
+import { Link } from "react-router-dom";
 import { MemoryRouter } from "react-router";
-import configureStore from 'redux-mock-store';
-import thunk from 'redux-thunk';
-import Navbar from './Navbar';
-import { Provider } from 'react-redux';
-import { NetworkConnectionProvider } from '../contexts/NetworkConnectionContext';
+import configureStore from "redux-mock-store";
+import thunk from "redux-thunk";
+import Navbar from "./Navbar";
+import { Provider } from "react-redux";
+import { NetworkConnectionProvider } from "../contexts/NetworkConnectionContext";
 
-const store = configureStore([
-  thunk,
-])({
+const store = configureStore([thunk])({
   users: {
     isLoggedIn: true,
     loading: false,
-    error: null
-  }
+    error: null,
+    userProfile: {
+      role: "USER",
+    },
+  },
 });
 
-describe('Navbar', () => {
-  it('should include links to home page and new article page', () => {                                       
+describe("Navbar", () => {
+  it("should include links to home page and new article page", () => {
     const component = mount(
       <Provider store={store}>
         <NetworkConnectionProvider>
@@ -29,7 +30,15 @@ describe('Navbar', () => {
         </NetworkConnectionProvider>
       </Provider>
     );
-    expect(component).toContainReact(<Link className="navbar__link" to={{ pathname: "/" }}>Home page</Link>);
-    expect(component).toContainReact(<Link className="navbar__link" to={{pathname: "/article/new" }}>New article</Link>);
+    expect(component).toContainReact(
+      <Link className="navbar__link" to={{ pathname: "/" }}>
+        Home page
+      </Link>
+    );
+    expect(component).toContainReact(
+      <Link className="navbar__link" to={{ pathname: "/article/new" }}>
+        New article
+      </Link>
+    );
   });
 });
