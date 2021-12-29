011de59c90cf7949f9f981e15842ab41e580ae71
Ewa Wydra <e.wydra@selleo.com>
2021-12-30 00:03:26 +0100

    Updates

---
+++ precache-manifest.892dacee57140f8b5f321fb52d6e0a21.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf59d4b91c3cb88dd8b2835958d9b34e",
+    "url": "/blog/index.html"
+  },
+  {
+    "revision": "6a4dba6ebaee44aefd52",
+    "url": "/blog/static/css/main.9c19dd1a.chunk.css"
+  },
+  {
+    "revision": "62d1f835db49cd8131f9",
+    "url": "/blog/static/js/2.8daace12.chunk.js"
+  },
+  {
+    "revision": "57153a035ba960c0e3086deffbe18f1f",
+    "url": "/blog/static/js/2.8daace12.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a4dba6ebaee44aefd52",
+    "url": "/blog/static/js/main.1d45d5c6.chunk.js"
+  },
+  {
+    "revision": "042e0e1222fb6f3edc08",
+    "url": "/blog/static/js/runtime-main.1dc37962.js"
+  }
+]);
